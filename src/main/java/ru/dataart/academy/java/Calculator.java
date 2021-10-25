a6f4bbe803a45af3fa1978293aec0edcdde513d9
tsragravorogh <71145107+tsragravorogh@users.noreply.github.com>
2021-10-25 10:48:49 +0300

    5(4) homework is done

---
+++ src/main/java/ru/dataart/academy/java/Calculator.java
@@ -0,0 +1,73 @@
+package ru.dataart.academy.java;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class Calculator {
+    /**
+     * @param zipFilePath -  path to zip archive with text files
+     * @param character   - character to find
+     * @return - how many times character is in files
+     */
+    public Integer getNumberOfChar(String zipFilePath, char character) {
+        Integer count = 0;
+
+        try {
+            final ZipFile zipFile = new ZipFile(zipFilePath);
+            final Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+            while (entries.hasMoreElements()) {
+                final ZipEntry zipEntry = entries.nextElement();
+                InputStream input = zipFile.getInputStream(zipEntry);
+
+                BufferedReader bf = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+                int symbol = bf.read();
+                while (symbol != -1) {
+                    if (character == (char) symbol) {
+                        count++;
+                    }
+                    symbol = bf.read();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    /**
+     * @param zipFilePath - path to zip archive with text files
+     * @return - max length
+     */
+
+    public Integer getMaxWordLength(String zipFilePath) {
+        int count = 0;
+
+        try {
+            final ZipFile zipFile = new ZipFile(zipFilePath);
+            final Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+            while (entries.hasMoreElements()) {
+                final ZipEntry zipEntry = entries.nextElement();
+                InputStream input = zipFile.getInputStream(zipEntry);
+
+                BufferedReader bf = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+                String line;
+                while ((line = bf.readLine()) != null) {
+                    String[] words = line.split(" ");
+                    for (String w : words) {
+                        if(w.length() > count) {
+                            count = w.length();
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+}
